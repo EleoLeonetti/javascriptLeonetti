0a1a238b3e7dfb7150f7de4dc246c77e7701be48
Eleonora Leonetti <eleo.leonetti@gmail.com>
2022-09-12 19:11:33 -0300

    desafio entregable 2

--- archivo.js
+++ archivo.js
@@ -1,24 +1,39 @@
-function sumar (producto1, producto2){
-    return producto1 + producto2;
-}
-let total = sumar(1000, 2000);
-alert("El total de su compra es: $ " + total);
-
-let descuento20 = total - (total * 0.20);
-
-alert("Descuento 20% por pago en efectivo o transferencia")
-let medioDePago = prompt("Seleccione forma de pago: 1.Efectivo 2. Transferencia 3. Tarjeta de debito, 4. Tarjeta de credito");
-
-function descuento (){
-    if ((medioDePago == "1") || (medioDePago == "2")){
-        return descuento20;
-    }else if ((medioDePago == "3") || (medioDePago == "4")){
-        alert("El total de su compra es: $ " + total);
-    }else{
-        alert("El método de pago ingresado no es válido")
+class Productos {
+    constructor(nombre, precio, tipo, disponible){
+        this.nombre = nombre;
+        this.precio = Number(precio);
+        this.tipo = tipo;
+        this.disponible = disponible;
     }
 }
-alert(descuento());
+const lista =  [
+new Productos ("Silla Mara", 4500, "muebles", true),
+new Productos ("Silla Taruca", 4500, "muebles", true),
+new Productos ("Silla Yaguarete", 4500, "muebles", true),
+new Productos ("Silla Yurumi", 4500, "muebles", true),
+new Productos ("Mesa Ombu", 5000, "muebles", true),
+new Productos ("Baul Ballena", 4500, "muebles", true),
+new Productos ("Rompecabezas Taruca", 800, "juegos", true),
+new Productos ("Rompecabezas Aguara Guazu", 800, "juegos", true),
+new Productos ("Rompecabezas Yaguarete", 800, "juegos", true),
+new Productos ("Kit Completo", 3900, "juegos", true),
+]
+
+const muebles = lista.slice(0, 6);
+const juegos = lista.slice(6, 10);
+
+function seleccion () {
+    let entrada = prompt("Opcion 1-Muebles / 2-Juegos")
+    if(entrada == 1){
+        console.log(muebles);
+    }if( entrada == 2){
+        console.log(juegos)
+    }else{
+        console.log("Error")
+    }
+}
+
+seleccion();
 
 
 
